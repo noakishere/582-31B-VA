a3db60c4786461b7e80bd89f510c25ea3708a9c9
kamyar <kamyarkarimi98@gmail.com>
2026-04-28 09:49:27 -0400

    class03 init

---
+++ class03/exercise/ex01.py
@@ -0,0 +1,8 @@
+# create a class of Fruit
+    # give it 3 attributes (1 should be price)
+    # define 3 methods that access the attributes and return them
+    # define a method that changes the price to a new price (taken as an parameter)
+        # and returns the new price
+
+
+
