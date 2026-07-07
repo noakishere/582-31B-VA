a81e2095b9dd9b3476ade3fcee627f51aa85dd24
kamyar <kamyarkarimi98@gmail.com>
2026-07-07 10:52:56 -0400

    search blueprint

--- class19/bp_app/team_routes.py
+++ class19/bp_app/team_routes.py
@@ -6,7 +6,9 @@ WORLD_CUP_TEAMS = [
     {"name": "Argentina", "group": "A"},
     {"name": "France", "group": "B"},
     {"name": "England", "group": "C"},
-    {"name": "Spain", "group": "D"}
+    {"name": "Spain", "group": "D"},
+    {"name": "Canada", "group": "A"},
+    {"name": "Portugal", "group": "B"},
 ]
 
 @teams.route("/teams")
@@ -23,7 +25,17 @@ def team_detail():
 @teams.route("/search")
 def search():
     group = request.args.get("group", "")
-    filtered = [team for team in WORLD_CUP_TEAMS if team["group"] == group] if group else WORLD_CUP_TEAMS
+    # filtered = [team for team in WORLD_CUP_TEAMS if team["group"] == group] if group else WORLD_CUP_TEAMS
+
+    if group: # is group empty/null or not !
+        filtered = [] # initialize the filtered list
+
+        for team in WORLD_CUP_TEAMS: # go through each element
+            if team["group"] == group: # if the criteria matches
+                filtered.append(team) # then, add it to the list
+    else: # if no query
+        filtered = WORLD_CUP_TEAMS # use the whole list
+
 
     return render_template("search.html", teams= filtered, group= group)
 
