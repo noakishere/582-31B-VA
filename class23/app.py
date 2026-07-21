482381880e7e509b9058efae97e75c9fead8cbe7
kamyar <kamyarkarimi98@gmail.com>
2026-07-21 10:19:59 -0400

    password validation

--- class23/app.py
+++ class23/app.py
@@ -27,3 +27,39 @@ login_manager.init_app(app)
 with app.app_context():
     # create the tables
     db.create_all()
+
+#### Loading User
+# Let's say:
+# session contains: "4"
+# user_loader receives "4"
+# Database query returns: Member object with ID 4
+# current_user becomes: that Member object
+@login_manager.user_loader
+def load_user(user_id):
+    return db.session.get(Member, int(user_id))
+
+#### Registeration Validation
+def validate_password(password):
+    # password: between 8 and 20 chars
+    if len(password) < 8:
+        return("Password must contain at least 8 characters.")
+    
+    if len(password) > 20:
+        return("Password must contain at most 20 characters.")
+    
+    # must have one uppercase at least
+    if not any(character.isupper() for character in password):
+        return("Password must contain at least an uppercase letter.")
+    
+    # password must have a number in it
+    if not any(character.isdigit() for character in password):
+        return("Password must contain a digit.")
+    
+    # This means password is valid
+    return None
+
+# for example:
+# password_error = validate_password("gjlksgjfs")
+# if password_error:
+#     flash(password_error, "error")
+
